9f513fa5e791a75f44a3d76fd6cac74f1ec956fc
Manuel Junior <manuel.jr@outlook.com>
2025-03-16 08:31:44 +0000

    Alter imports constants

--- src/auth/guard/jwt.guard.ts
+++ src/auth/guard/jwt.guard.ts
@@ -1,7 +1,7 @@
 
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Constants } from 'utils/constants';
+import { Constants } from '../../utils/constants';
 
 
 
